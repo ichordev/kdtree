f23aa6c5e2a78bcb654a78710f1d38152508527a
diddiz <robin.kupper@rwth-aachen.de>
2017-12-14 16:37:01 +0100

    Refactored benchmarks.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.datetime.stopwatch : benchmark;
 import kdtree;
 
 void main() {
@@ -6,15 +7,8 @@ void main() {
 	benchmarkGrowing();
 }
 
-void benchmarkNearest() {
-	import fluent.asserts : should;
-	import std.stdio : writeln;
+private double[dim][] randomPoints(size_t dim)(size_t numPoints) {
 	import std.random : uniform01;
-	import std.datetime.stopwatch : benchmark;
-
-	enum dim = 3;
-	enum numPoints = 1000;
-	enum numTestPoints = 100;
 
 	auto points = new double[dim][numPoints];
 	foreach (i; 0 .. points.length) {
@@ -22,45 +16,91 @@ void benchmarkNearest() {
 			points[i][j] = uniform01;
 		}
 	}
-	auto testPoints = new double[dim][numTestPoints];
-	foreach (i; 0 .. testPoints.length) {
-		foreach (j; 0 .. testPoints[i].length) {
-			testPoints[i][j] = uniform01;
+	return points;
+}
+
+private double[5][] pointsFromFile(string path, size_t numPoints) {
+	import std.algorithm : joiner;
+	import std.csv : csvReader;
+	import std.typecons : Tuple;
+
+	double[5][] points;
+	auto file = File(path, "r");
+	scope (exit)
+		file.close();
+
+	foreach (record; file.byLine.joiner("\n").csvReader!(Tuple!(double, double, double, double, double))) {
+		double[5] point = [record[0], record[1], record[2], record[3], record[4]];
+		points ~= point;
+
+		if (points.length == numPoints) {
+			break;
 		}
 	}
+	return points;
+}
 
+/// Test pointsFromFile
+unittest {
+	import fluent.asserts : should;
+
+	auto points = pointsFromFile("species.csv", 10);
+
+	points.length.should.equal(10);
+	points[0].should.equal([0, 0.4474427700042725, 0, 0.9130669832229614, 0.9997519850730896]);
+	points[$ - 1].should.equal([3.31227707862854, 0.9355245232582092, 0.4000000059604645, 0.9130669832229614, 0.9997519850730896]);
+}
+
+/**
+	Benchmarks finding x nearest neighbors of y points.
+	Benchmarks include time for creating the kd trees, if used.
+	A dataset of random points is used.
+*/
+void benchmarkNearest() {
+	import fluent.asserts : should;
+
+	enum dim = 3;
+	enum numPoints = 1000;
+	enum numTestPoints = 100;
+
+	auto points = randomPoints!dim(numPoints);
+	auto testPoints = randomPoints!dim(numTestPoints);
+
+	/// Find nearest neighbour naively by comparing all distances.
 	static double nearestNaive(size_t k, T)(T[k][] points, in T[k][] testPoints) {
 		import std.algorithm : minElement;
 		import std.numeric : euclideanDistance;
 
 		auto sum = 0.0;
-		foreach (point; testPoints) {
+		foreach (ref point; testPoints) {
 			auto nearest = points.minElement!(a => a[].euclideanDistance(point[]));
 			sum += nearest[0];
 		}
 		return sum;
 	}
 
+	/// Create kdtree knowing all points.
 	static double nearestKdTree(size_t k, T)(T[k][] points, in T[k][] testPoints) {
 		auto root = kdTree(points);
 
 		auto sum = 0.0;
-		foreach (point; testPoints) {
+		foreach (ref point; testPoints) {
 			auto nearest = root.nearest(point);
 			sum += nearest[0];
 		}
 		return sum;
 	}
 
+	/// Grow kdtree by adding all points. If data is biased, the tree should become unbalanced.
 	static double nearestKdTreeAdd(size_t k, T)(T[k][] points, in T[k][] testPoints) {
 		auto root = kdTree!(k, T);
 
-		foreach (point; points) {
+		foreach (ref point; points) {
 			root.add(point);
 		}
 
 		auto sum = 0.0;
-		foreach (point; testPoints) {
+		foreach (ref point; testPoints) {
 			auto nearest = root.nearest(point);
 			sum += nearest[0];
 		}
@@ -75,6 +115,11 @@ void benchmarkNearest() {
 	writeln("Naive: ", results[0], " ", " Kd: ", results[1], " Kd-Add: ", results[2]);
 }
 
+/**
+	Benchmarks finding nearest neighbors of points while adding these points to the set after adding.
+	Benchmarks include time for creating the kd trees, if used.
+	A dataset of random points is used.
+*/
 void benchmarkGrowing() {
 	import fluent.asserts : should;
 	import std.stdio : writeln;
@@ -84,61 +129,54 @@ void benchmarkGrowing() {
 	enum dim = 5;
 	enum numPoints = 1000;
 
-	auto points = new double[dim][numPoints];
-	foreach (i; 0 .. points.length) {
-		foreach (j; 0 .. points[i].length) {
-			points[i][j] = uniform01;
-		}
-	}
-	auto testPoints = new double[dim][numPoints];
-	foreach (i; 0 .. testPoints.length) {
-		foreach (j; 0 .. testPoints[i].length) {
-			testPoints[i][j] = uniform01;
-		}
-	}
+	auto points = randomPoints!dim(numPoints);
 
-	static double nearestNaive(size_t k, T)(T[k][] points, in T[k][] testPoints) {
+	/// Find nearest neighbour naively by comparing all distances.
+	static double nearestNaive(size_t k, T)(T[k][] points) {
 		import std.algorithm : minElement;
 		import std.numeric : euclideanDistance;
 
 		auto sum = 0.0;
-		foreach (i; 0 .. points.length) {
-			auto nearest = points[0 .. i + 1].minElement!(a => a[].euclideanDistance(testPoints[i][]));
+		foreach (i; 1 .. points.length) {
+			auto nearest = points[0 .. i].minElement!(a => a[].euclideanDistance(points[i][]));
 			sum += nearest[0];
 		}
 		return sum;
 	}
 
-	static double nearestAdd(size_t k, T)(T[k][] points, in T[k][] testPoints) {
-		auto root = kdTree!(k, T);
+	/// Use kd tree and add each point after finding the nearest neighbor.
+	static double nearestAdd(size_t k, T)(T[k][] points) {
+		auto root = kdTree([points[0]]);
 
 		auto sum = 0.0;
-		foreach (i; 0 .. points.length) {
+		foreach (i; 1 .. points.length) {
+			auto nearest = root.nearest(points[i]);
 			root.add(points[i]);
-			auto nearest = root.nearest(testPoints[i]);
 			sum += nearest[0];
 		}
 		return sum;
 	}
 
-	static double nearestAddRebalance(size_t k, T)(T[k][] points, in T[k][] testPoints) {
-		auto root = kdTree!(k, T);
+	/// Use kd tree and add each point after finding the nearest neighbor. Repalances the kd tree after 100000 insertions.
+	static double nearestAddRebalance(size_t k, T)(T[k][] points) {
+		auto root = kdTree([points[0]]);
 
 		auto sum = 0.0;
-		foreach (i; 0 .. points.length) {
+		foreach (i; 1 .. points.length) {
+			auto nearest = root.nearest(points[i]);
+
 			root.add(points[i]);
 			if (i % 100000 == 0) {
 				root.rebalance();
 			}
 
-			auto nearest = root.nearest(testPoints[i]);
 			sum += nearest[0];
 		}
 		return sum;
 	}
 
 	double sum1 = 0, sum2 = 0, sum3 = 0;
-	auto results = benchmark!({ sum1 += nearestNaive(points, testPoints); }, { sum2 += nearestAdd(points, testPoints); }, { sum3 += nearestAddRebalance(points, testPoints); })(1);
+	auto results = benchmark!({ sum1 += nearestNaive(points); }, { sum2 += nearestAdd(points); }, { sum3 += nearestAddRebalance(points); })(1);
 	sum2.should.equal(sum1);
 	sum3.should.equal(sum1);
 
